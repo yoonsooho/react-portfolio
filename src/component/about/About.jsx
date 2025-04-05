f0cc967c111c5bb6bac9a67cee06fa761f9c97fa
yoonsooho <7552678@naver.com>
2025-04-05 20:58:20 +0900

    chore : 디자인 변경

--- src/component/about/About.jsx
+++ src/component/about/About.jsx
@@ -1,10 +1,9 @@
+import { motion } from "framer-motion";
 import React from "react";
 import { FaAward } from "react-icons/fa";
 import { FiUsers } from "react-icons/fi";
 import ME from "../../assets/photo.png";
 import classes from "./About.module.css";
-import { motion } from "framer-motion";
-
 // 애니메이션 변수들을 따로 분리
 const floatAnimation = {
     animate: { y: [0, -10, 0] },
@@ -76,8 +75,8 @@ const About = () => {
                     </div>
 
                     <div className={classes.about__content__right}>
-                        <p>✔️ 문제 해결력이 뛰어난 프론트엔드 개발자를 꿈꿉니다. </p>
-                        <p>✔️ 기술적인 주제에 대해 글 쓰는 것을 좋아하고 잘 합니다. </p>
+                        <p>✔️ 문제 해결력이 뛰어난 프론트엔드 개발자를 꿈꿉니다.</p>
+                        <p>✔️ 기술적인 주제에 대해 글 쓰는 것을 좋아하고 잘 합니다.</p>
                         <p>✔️ 간단한 용어로 설명해 사람들이 쉽게 이해하도록 하는 것에 보람을 느낍니다.</p>
                     </div>
                     <a href="#contact" className="btn btn-primary">
